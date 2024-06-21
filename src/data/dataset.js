fd0b45a3ffdb8dcececa0092a560e8be82b2dafa
Pamela Olguin <pame.ertek@gmail.com>
2024-06-20 21:57:28 -0400

    imagenes con IA

--- src/data/dataset.js
+++ src/data/dataset.js
@@ -4,7 +4,7 @@ export default [
     "name": "Ada Lovelace",
     "shortDescription": "Pionera en la programación, primera programadora de la historia.",
     "description": "Una visionaria matemática británica del siglo XIX, es aclamada como la primera programadora de la historia. Su colaboración con Charles Babbage en la creación de la Máquina Analítica la llevó a desarrollar algoritmos, incluyendo el famoso \"algoritmo de Bernoulli\", destinados a ser procesados por una máquina. Su visión pionera la convirtió en una figura fundamental en la informática y la inteligencia artificial. Lovelace demostró que las computadoras podían ir más allá de simples cálculos matemáticos, anticipando un mundo donde la programación sería esencial en la resolución de problemas complejos, dejando una huella imborrable en la ciencia y la tecnología.",
-    "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/ada-lovelace.jpg",
+    "imageUrl": "https://th.bing.com/th/id/OIG2.0ZM9XJvnSoMxH5_HSPe5?pid=ImgGn",
     "facts": {
       "yearOfBirth": "1815",
       "yearOfDeath": "1852",
@@ -20,7 +20,7 @@ export default [
     "name": "Emmy Noether",
     "shortDescription": "Matemática destacada, teorema fundamental en álgebra abstracta.",
     "description": "Una brillante matemática alemana, revolucionó la física teórica con sus contribuciones al teorema de conservación de la energía y el teorema de Noether, fundamentales en la teoría de la relatividad y la mecánica cuántica. A pesar de los obstáculos que enfrentó como mujer en la academia, sus investigaciones en álgebra abstracta y teoría de grupos son fundamentales en la física moderna. Su capacidad para unificar conceptos matemáticos y físicos marcó un hito en la historia de la ciencia, allanando el camino para futuras generaciones de científicas.",
-    "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/emmy-noether.jpg",
+    "imageUrl": "https://ciencias.uautonoma.cl/wp-content/uploads/2020/04/emmy.png",
     "facts": {
       "yearOfBirth": "1882",
       "yearOfDeath": "1935",
@@ -36,7 +36,7 @@ export default [
     "name": "Hypatia de Alejandría",
     "shortDescription": "Matemática y filósofa en la antigua Alejandría.",
     "description": "Filósofa y matemática en la antigua Alejandría, desafió las normas de género de su época al destacar en una sociedad dominada por hombres. Su legado incluye importantes contribuciones a la geometría y la astronomía, así como su destacado papel como maestra y defensora del conocimiento en una época de agitación política. Hypatia demostró que las mujeres podían sobresalir en campos intelectuales y su valentía al mantener sus creencias filosóficas la convierte en un símbolo de resistencia y sabiduría en la historia de la ciencia.",
-    "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/hypatia-de-alejandria.jpg",
+    "imageUrl": "https://th.bing.com/th/id/OIG1.cJIBrDrYJvUF.VOfuaDd?w=1024&h=1024&rs=1&pid=ImgDetMain",
     "facts": {
       "yearOfBirth": "circa 360",
       "yearOfDeath": "415",
@@ -52,7 +52,7 @@ export default [
     "name": "Marjorie Lee Browne",
     "shortDescription": "Matemática, contribuyó a la teoría de números.",
     "description": "Matemática estadounidense, destacó en una época en la que las mujeres afroamericanas enfrentaban múltiples barreras en la educación superior. No solo obtuvo su doctorado en matemáticas, sino que también se convirtió en una destacada educadora, promoviendo la diversidad en las ciencias matemáticas y desafiando la discriminación racial y de género. Su compromiso con la educación y su capacidad para inspirar a futuras generaciones la convierten en un modelo a seguir para las mujeres en la ciencia, dejando una huella perdurable en la historia académica.",
-    "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/marjorie-lee-browne.jpg",
+    "imageUrl": "https://th.bing.com/th/id/OIG1.uNb7uipgT7cH4UqUThOl?w=1024&h=1024&rs=1&pid=ImgDetMain",
     "facts": {
       "yearOfBirth": "1914",
       "yearOfDeath": "1979",
@@ -68,7 +68,7 @@ export default [
     "name": "Shakuntala Devi",
     "shortDescription": "Matemática prodigio, conocida como la 'Calculadora Humana'.",
     "description": "Conocida como \"la bruja de las matemáticas\" de la India, dejó una marca indeleble en el mundo de los números. Su capacidad mental asombrosa la llevó a resolver complejos cálculos matemáticos mentalmente en cuestión de segundos, estableciendo récords mundiales. Además de su talento innato, Devi promovió activamente la educación matemática y luchó por la igualdad de género en un país donde las mujeres enfrentaban desafíos en la ciencia y la educación. Su legado destaca el poder de la mente humana y la importancia de la igualdad de oportunidades en la ciencia.",
-    "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/shakuntala-devi.jpg",
+    "imageUrl": "https://static01.nyt.com/images/2013/04/24/world/asia/devi-1/devi-1-superJumbo.jpg",
     "facts": {
       "yearOfBirth": "1929",
       "yearOfDeath": "2013",
@@ -84,7 +84,7 @@ export default [
     "name": "Sofia Kovalevskaya",
     "shortDescription": "Matemática y escritora, contribuyó a la teoría de funciones.",
     "description": "Matemática rusa del siglo XIX, dejó una marca indeleble en la ciencia del siglo XIX al desafiar las barreras de género. Como matemática rusa, superó las restricciones sociales de su época para convertirse en la primera mujer en obtener un doctorado en matemáticas en Europa. Sus contribuciones a la teoría de las ecuaciones diferenciales y la mecánica revolucionaron la disciplina, abriendo nuevas perspectivas en el análisis matemático. Su destacada membresía en la Academia de Ciencias de Suecia marcó un hito en la historia de las mujeres en la ciencia.",
-    "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/sofia-kovalevskaya.jpg",
+    "imageUrl": "https://th.bing.com/th/id/OIG4.gEsjmMt1in1ojPGj95e8?w=1024&h=1024&rs=1&pid=ImgDetMain",
     "facts": {
       "yearOfBirth": "1850",
       "yearOfDeath": "1891",
@@ -100,7 +100,7 @@ export default [
     "name": "Yoko Shimomura",
     "shortDescription": "Compositora de música, destacada en la industria de los videojuegos.",
     "description": "Compositora japonesa, es una figura destacada en la ciencia de la música y el arte sonoro. Aunque no se dedica a la ciencia en el sentido tradicional, su trabajo en la composición musical ha requerido un profundo entendimiento de la teoría musical y la tecnología de audio. Ha contribuido de manera significativa a la industria de los videojuegos, componiendo icónicas bandas sonoras que han enriquecido la experiencia de juego. Su éxito en un campo donde las mujeres estaban subrepresentadas la convierte en un modelo a seguir, incluso en campos no convencionales como la composición musical.",
-    "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/yoko-shimomura.jpg",
+    "imageUrl": "https://th.bing.com/th/id/OIG3.tdI1vIv9Ek6kIwFFjDGg?w=1024&h=1024&rs=1&pid=ImgDetMain",
     "facts": {
       "yearOfBirth": "1967",
       "birthPlace": "Hyogo, Japón",
@@ -115,7 +115,7 @@ export default [
     "name": "Maryam Mirzakhani",
     "shortDescription": "Matemática, ganadora del Premio Fields.",
     "description": "Matemática iraní, hizo historia al convertirse en la primera mujer en ganar la Medalla Fields, el premio más prestigioso en matemáticas. Sus investigaciones en la geometría de superficies y la teoría de las superficies de Riemann han tenido un impacto duradero en la matemática pura. A lo largo de su carrera, Mirzakhani inspiró a muchas mujeres a perseguir carreras en ciencia y matemáticas, derribando las barreras de género. Su enfoque innovador y su capacidad para resolver problemas complejos la convierten en un modelo a seguir para la comunidad científica y en un símbolo de la excelencia matemática.",
-    "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/maryam-mirzakhani.jpg",
+    "imageUrl": "https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2017/07/15/15001170322481.jpg",
     "facts": {
       "yearOfBirth": "1977",
       "yearOfDeath": "2017",
