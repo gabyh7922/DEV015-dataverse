6ce4449e61850f735efbd1e9132a43bfb25144c2
Pamela Olguin <pame.ertek@gmail.com>
2024-07-01 10:12:13 -0400

    indentación corregido

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import { renderCard } from './view.js'; //importa view
 const root = document.querySelector("#root"); //constante que selecciona el div
 
 for (let i = 0; i < data.length; i++) { //for que recorre mi data
-    if (i in [2,4,5,6,7,8,9,10]){;  //aca estas trabajando filtro
-        root.appendChild(renderCard(data[i]));  // muestra el resultado y se crea en base al recorrido
-    }
+  if (i in [2,4,5,6,7,8,9,10]){
+    root.appendChild(renderCard(data[i]));  // muestra el resultado y se crea en base al recorrido
+  }
 }
 
 
--- src/view.js
+++ src/view.js
@@ -1,34 +1,34 @@
 
 
 export const renderCard = (card) => {  //creando la constante para importarla en el main
-    const div = document.createElement("div"); //creando el div
-    div.classList.add("card"); //agregando la clase card
-    
-    const ul = document.createElement("ul"); //creando el ul
+  const div = document.createElement("div"); //creando el div
+  div.classList.add("card"); //agregando la clase card
 
-    const datos = `
-        <li itemprop="name">Nombre: ${card['name']}</li>
-        <li itemprop="shortDescription">Description: ${card['shortDescription']}</li>
-        <li itemprop="yearOfBirth">Año de Nacimiento: ${card['facts']['yearOfBirth']}</li>
-        <li itemprop="yearOfDeath">Fecha de Muerte: ${card['facts']['yearOfDeath']}</li>
-        <li itemprop="birthPlace">Lugar de Nacimiento: ${card['facts']['birthPlace']}</li>
-        <li itemprop="mainField">Especialida: ${card['facts']['mainField']}</li>
-    `;//creando tarjeta que contiene informacion, debe ser trabajada en css
+  const ul = document.createElement("ul"); //creando el ul
 
-    const info = `
-    <p itemprop="imageUrl"><img src="${card['imageUrl']}" /></p>
-        `;
+  const datos = `
+    <li itemprop="name">Nombre: ${card['name']}</li>
+    <li itemprop="shortDescription">Description: ${card['shortDescription']}</li>
+    <li itemprop="yearOfBirth">Año de Nacimiento: ${card['facts']['yearOfBirth']}</li>
+    <li itemprop="yearOfDeath">Fecha de Muerte: ${card['facts']['yearOfDeath']}</li>
+    <li itemprop="birthPlace">Lugar de Nacimiento: ${card['facts']['birthPlace']}</li>
+    <li itemprop="mainField">Especialida: ${card['facts']['mainField']}</li>
+`;//creando tarjeta que contiene informacion, debe ser trabajada en css
 
-    ul.innerHTML = datos; //indicandole al ul que contendra el bloque html de la tarjeta
-    
+  const info = `
+<p itemprop="imageUrl"><img src="${card['imageUrl']}" /></p>
+    `;
 
-    const infoDiv = document.createElement("div"); //creando el div
-    infoDiv.innerHTML = info; //indicandole al div que contendra el bloque html de la imagen
-    div.appendChild(infoDiv);
+  ul.innerHTML = datos; //indicandole al ul que contendra el bloque html de la tarjeta
 
-    div.appendChild(ul); //pegandole un hijo al div, asignandlo ul como hijo al div
 
-    return div; //retornando div
+  const infoDiv = document.createElement("div"); //creando el div
+  infoDiv.innerHTML = info; //indicandole al div que contendra el bloque html de la imagen
+  div.appendChild(infoDiv);
+
+  div.appendChild(ul); //pegandole un hijo al div, asignandlo ul como hijo al div
+
+  return div; //retornando div
     
 }
 
