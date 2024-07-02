3e02837be7dc75e46a68402110477011e007c482
Pamela Olguin <pame.ertek@gmail.com>
2024-07-01 22:01:53 -0400

    filtrar por categoria ok. sin select de categoria aun.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import data from './data/dataset.js'; //importa data
 import { renderItems } from './view.js'; //importa view
 
-renderItems(data);
+renderItems(data,"Matemáticas","asc");
 
 
 
--- src/view.js
+++ src/view.js
@@ -1,13 +1,16 @@
 import { renderCard } from './card.js'; //importa view
 
-export const renderItems = (data,filterBy='category',orderBy='asc') => {
+export const renderItems = (data,filterBy=null,orderBy='asc') => {
   
   const root = document.querySelector("#root"); //constante que selecciona el div
 
   for (let i = 0; i < data.length; i++) {
   
-      const card = renderCard(data[i]);
-      root.appendChild(card);
+      const cardData = data[i];
+      if( (filterBy !== null && cardData['facts']['mainField'].includes(filterBy)) || filterBy === null){
+        const card = renderCard(cardData);
+        root.appendChild(card);
+      }
   }
 }
 
