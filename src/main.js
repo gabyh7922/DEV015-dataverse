ecb67033298f35b0be613fe66e3c205d0ece3c17
Pamela Olguin <pame.ertek@gmail.com>
2024-07-01 22:30:46 -0400

    ordenar por nombre ok. sin select de ordenar por todavia

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import data from './data/dataset.js'; //importa data
 import { renderItems } from './view.js'; //importa view
 
-renderItems(data,"Matemáticas","asc");
-
-
-
+renderItems(data,null,"asc");
 
  
--- src/view.js
+++ src/view.js
@@ -4,13 +4,32 @@ export const renderItems = (data,filterBy=null,orderBy='asc') => {
   
   const root = document.querySelector("#root"); //constante que selecciona el div
 
+  const filtered = [];// array que contendra datos filtrados
+
+  // filtrar tarjetas
   for (let i = 0; i < data.length; i++) {
   
-      const cardData = data[i];
-      if( (filterBy !== null && cardData['facts']['mainField'].includes(filterBy)) || filterBy === null){
-        const card = renderCard(cardData);
-        root.appendChild(card);
-      }
+    const cardData = data[i];
+    if( (filterBy !== null && cardData['facts']['mainField'].includes(filterBy)) || filterBy === null){
+      filtered.push(cardData);
+    }
+  }
+
+  // ordenar por nombre
+  if( orderBy === 'desc'){
+    // ordenar descendiente
+    filtered.sort((a,b)=>b.name.localeCompare(a.name));
+  }
+  else{
+    // ordenar ascendiente
+    filtered.sort((a,b)=>a.name.localeCompare(b.name));
+  }
+
+  // imprimir tarjetas
+  for (let i = 0; i < filtered.length; i++) {
+
+      const card = renderCard(filtered[i]);
+      root.appendChild(card);
   }
 }
 
