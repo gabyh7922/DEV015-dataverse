794a196abd1bbe3461b24d16b930eba633717eca
Pamela Olguin <pame.ertek@gmail.com>
2024-07-02 21:48:27 -0400

    se agrego test data

--- test/data.js
+++ test/data.js
@@ -1,5 +1,32 @@
 // Aqui puedes agregar los datos que necesites para tus pruebas
 // al menos debe tener uno que se llame "data"
 
-export const data = [];
+export const data = [
+  {
+    "id": "ada-lovelace",
+    "name": "Ada Lovelace",
+    "shortDescription": "Pionera en la programación, primera programadora de la historia.",
+    "description": "Una visionaria matemática británica del siglo XIX, es aclamada como la primera programadora de la historia. Su colaboración con Charles Babbage en la creación de la Máquina Analítica la llevó a desarrollar algoritmos, incluyendo el famoso \"algoritmo de Bernoulli\", destinados a ser procesados por una máquina. Su visión pionera la convirtió en una figura fundamental en la informática y la inteligencia artificial. Lovelace demostró que las computadoras podían ir más allá de simples cálculos matemáticos, anticipando un mundo donde la programación sería esencial en la resolución de problemas complejos, dejando una huella imborrable en la ciencia y la tecnología.",
+    "imageUrl": "https://th.bing.com/th/id/OIG2.0ZM9XJvnSoMxH5_HSPe5?",
+    "facts": {
+      "yearOfBirth": "1815",
+      "yearOfDeath": "1852",
+      "birthPlace": "Londres, Reino Unido",
+      "mainField": ["Ciencias de la Computación"]
+    }
+  },
+  {
+    "id": "emmy-noether",
+    "name": "Emmy Noether",
+    "shortDescription": "Matemática destacada, teorema fundamental en álgebra abstracta.",
+    "description": "Una brillante matemática alemana, revolucionó la física teórica con sus contribuciones al teorema de conservación de la energía y el teorema de Noether, fundamentales en la teoría de la relatividad y la mecánica cuántica. A pesar de los obstáculos que enfrentó como mujer en la academia, sus investigaciones en álgebra abstracta y teoría de grupos son fundamentales en la física moderna. Su capacidad para unificar conceptos matemáticos y físicos marcó un hito en la historia de la ciencia, allanando el camino para futuras generaciones de científicas.",
+    "imageUrl": "https://ciencias.uautonoma.cl/wp-content/uploads/2020/04/emmy.png",
+    "facts": {
+      "yearOfBirth": "1882",
+      "yearOfDeath": "1935",
+      "birthPlace": "Erlangen, Alemania",
+      "mainField": ["Matemáticas"]
+    }
+  }
+];
 
