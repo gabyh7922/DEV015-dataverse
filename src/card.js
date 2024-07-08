11a4ba03220f601bb9df1058b98045ebfdb0c268
Pamela Olguin <pame.ertek@gmail.com>
2024-07-07 23:03:10 -0400

    mejoramiento de css del botón y de los select para pasar test

--- src/card.js
+++ src/card.js
@@ -8,13 +8,13 @@ export const renderCard = (card) => {  //creando la constante para importarla en
   // revisamos si tiene fecha de la muerte. porque no todos tienen este dato
   const yearOfDeath = card['facts']['yearOfDeath'] ? card['facts']['yearOfDeath'] : '-';
   const datos = `
-    <li itemtype="imageUrl" itemscope="imageUrl"><img src="${card['imageUrl']}" /></li>
-    <li itemtype="name" itemscope="name">Nombre: ${card['name']}</li>
-    <li itemtype="shortDescription" itemscope="shortDescription">Description: ${card['shortDescription']}</li>
-    <li itemtype="yearOfBirth" itemscope="yearOfBirth">Año de Nacimiento: ${card['facts']['yearOfBirth']}</li>
-    <li itemtype="yearOfDeath" itemscope="yearOfDeath">Fecha de Muerte: ${yearOfDeath}</li>
-    <li itemtype="birthPlace" itemscope="birthPlace">Lugar de Nacimiento: ${card['facts']['birthPlace']}</li>
-    <li itemtype="mainField" itemscope="mainField">Especialida: ${card['facts']['mainField']}</li>
+    <li itemtype="imageUrl" itemscope="imageUrl" class="item"><img src="${card['imageUrl']}" /></li>
+    <li itemtype="name" itemscope="name" class="item">Nombre: ${card['name']}</li>
+    <li itemtype="shortDescription" itemscope="shortDescription" class="item">Description: ${card['shortDescription']}</li>
+    <li itemtype="yearOfBirth" itemscope="yearOfBirth" class="item">Año de Nacimiento: ${card['facts']['yearOfBirth']}</li>
+    <li itemtype="yearOfDeath" itemscope="yearOfDeath" class="item">Fecha de Muerte: ${yearOfDeath}</li>
+    <li itemtype="birthPlace" itemscope="birthPlace" class="item">Lugar de Nacimiento: ${card['facts']['birthPlace']}</li>
+    <li itemtype="mainField" itemscope="mainField" class="item">Especialida: ${card['facts']['mainField']}</li>
 `;//creando tarjeta que contiene informacion, debe ser trabajada en css
 
   ul.innerHTML = datos; //indicandole al ul que contendra el bloque html de la tarjeta
