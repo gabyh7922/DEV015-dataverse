bf1b2efd1da05c99b7247d4976e78b7f86aa7464
Pamela Olguin <pame.ertek@gmail.com>
2024-07-09 17:08:42 -0400

    corrección de test js forEach

--- src/view.js
+++ src/view.js
@@ -5,14 +5,13 @@ export const renderItems = (data,filterByCategory='',orderDir='asc') => {
   const filtered = [];// array que contendra datos filtrados
 
   // filtrar tarjetas
-  for (let i = 0; i < data.length; i++) {
-  
-    const cardData = data[i];
+  //for (let i = 0; i < data.length; i++) {
+  data.forEach((cardData)=>{
     // si estamos filtrando por una categoria especifica o la categria es vacia=''
     if( (filterByCategory !== '' && cardData['facts']['mainField'].includes(filterByCategory)) || filterByCategory === ''){
       filtered.push(cardData);
     }
-  }
+  });
 
   // ordenar por nombre
   if( orderDir === 'desc'){
